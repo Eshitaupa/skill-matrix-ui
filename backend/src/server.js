732331f1a8310efa0c1ed624d4c2c7f1ca929bbf
Upadhyaya <esupadhyaya@burnsmcd.com>
2026-08-20 09:42:41 +0530

    uat link added

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "https://skill-matrix-fhadc3d4c3g8dhcg.northcentralus-01.azurewebsites.net",
+  "https://skill-matrix-uat-g5dba9ate9eyhhhc.northcentralus-01.azurewebsites.net/",
 ];
 
 app.use(
